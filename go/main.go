6a95b3eb7b5b604c1bc427bcbe9c784147260d82
Daniel Aberger <d.aberger@reply.de>
2021-12-02 13:46:01 +0100

    remove dent from code

--- go/main.go
+++ go/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/voigt/adventofcode2021/pkg/advdentofcode/one"
-	"github.com/voigt/adventofcode2021/pkg/advdentofcode/two"
+	"github.com/voigt/adventofcode2021/pkg/adventofcode/one"
+	"github.com/voigt/adventofcode2021/pkg/adventofcode/two"
 	"github.com/voigt/adventofcode2021/pkg/utils"
 )
 
 func main() {
-	inputDay1 := utils.GetFileInputsAsIntSlice("pkg/advdentofcode/one/input.txt")
+	inputDay1 := utils.GetFileInputsAsIntSlice("pkg/adventofcode/one/input.txt")
 	fmt.Printf("Day 1 - Result PartOne: %d\n", one.PartOne(inputDay1))
 	fmt.Printf("Day 1 - Result PartTwo: %d\n", one.PartTwo(inputDay1))
 	fmt.Println("------------")
-	inputDay2 := utils.GetFileInputsAsStrSlice("pkg/advdentofcode/two/input.txt")
+	inputDay2 := utils.GetFileInputsAsStrSlice("pkg/adventofcode/two/input.txt")
 	fmt.Printf("Day 2 - Part One: %d\n", two.PartTwo(inputDay2))
 	fmt.Printf("Day 2 - Part Two: %d\n", two.PartTwo(inputDay2))
 }
