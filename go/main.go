eda5beda54b7626e0281080dff5a075d4cf78ce7
Christoph Voigt <voigt.christoph@gmail.com>
2021-12-07 17:23:09 +0100

    add stuff

--- go/main.go
+++ go/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 
+	"github.com/voigt/adventofcode2021/pkg/adventofcode/four"
 	"github.com/voigt/adventofcode2021/pkg/adventofcode/one"
+	seven "github.com/voigt/adventofcode2021/pkg/adventofcode/seven"
 	"github.com/voigt/adventofcode2021/pkg/adventofcode/three"
 	"github.com/voigt/adventofcode2021/pkg/adventofcode/two"
 	"github.com/voigt/adventofcode2021/pkg/utils"
@@ -21,4 +23,12 @@ func main() {
 	inputDay3 := utils.GetFileInputsAsStrSlice("pkg/adventofcode/three/input.txt")
 	fmt.Printf("Day 3 - Part One: %d\n", three.PartOne(inputDay3))
 	fmt.Printf("Day 3 - Part Two: %d\n", three.PartTwo(inputDay3))
+	fmt.Println("------------")
+	inputDay4 := utils.GetFileInputsAsStrSlice("pkg/adventofcode/four/input.txt")
+	fmt.Printf("Day 4 - Part One: %d\n", four.PartOne(inputDay4))
+	fmt.Printf("Day 4 - Part Two: %d\n", four.PartTwo(inputDay4))
+	fmt.Println("------------")
+	inputDay7 := utils.GetFileInputsAsStrSlice("pkg/adventofcode/seven/input.txt")
+	fmt.Printf("Day 7 - Part One: %d\n", seven.PartOne(inputDay7))
+	fmt.Printf("Day 7 - Part Two: %d\n", seven.PartTwo(inputDay7))
 }
